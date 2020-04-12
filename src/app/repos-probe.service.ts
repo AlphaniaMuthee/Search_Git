56620ed362277511098455ad4b29a65476c374f6
AlphaniaMuthee <walphania2@gmail.com>
2020-04-12 16:26:45 +0300

    add changes to classes

--- src/app/repos-probe.service.ts
+++ src/app/repos-probe.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import { environment } from '.././environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReposProbeService {
+export class ReposEnquiryService {
+  repos : any;
+  private Username : string;
 
-  constructor() { }
+  constructor( private http : HttpClient ) {
+  }
+  getrepos(){
+    let promise = new Promise((resolve)=>{
+
+    this.http.get("https://api.github.com/users/" + this.Username + "/repos?client_id =" + environment.clientId+ "&client_secret=" + environment.clientsecret).toPromise().then(repos =>{
+      this.repos = repos
+    resolve()
+    })
+    })
+    return promise
+  }
+  updateprofile(username : string){
+    this.Username = username
+
+  }
 }
