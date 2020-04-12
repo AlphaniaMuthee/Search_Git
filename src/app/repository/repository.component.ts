2c6449d5992896c94c2359df993299b1e09d5537
AlphaniaMuthee <walphania2@gmail.com>
2020-04-12 18:11:06 +0300

    add repository ts instructions

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Repo} from '../repo'
+import {ReposEnquiryService} from '../repos-probe.service';
+import {ServicesService} from '../userprofile/service.service';
 
 @Component({
   selector: 'app-repository',
@@ -7,8 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoryComponent implements OnInit {
 
-  constructor() { }
+  repos : Repo;
+  username : string;
 
+  constructor(public repos_get : ReposProbeService, public user : ServiceService) {
+  }
+  showRepos(){
+    this.repos_get.updateuserprofile(this.username)
+    this.repos_get.getrepos();
+    this.repos=this.repos_get.repos
+ }
   ngOnInit(): void {
   }
 
