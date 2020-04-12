3f65424b9eefe9a69fcc6908bce1cb9172d2e341
AlphaniaMuthee <walphania2@gmail.com>
2020-04-12 16:53:16 +0300

    add userprofile ts file updates

--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent implements OnInit {
+  username : string;
+  userprofiles : any;
 
-  constructor() { }
+  constructor(public findprofile : ServiceService) {
+
+  }
+  addUser(){
+    this.finduserprofile.updateuserprofile(this.username);
+    this.finduserprofile.getprofile();
+    this.userprofiles = this.finduserprofile.userprofiles
+    console.log(this.userprofiles)
+  }
 
   ngOnInit(): void {
   }
