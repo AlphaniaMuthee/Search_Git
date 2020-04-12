60daac7999b63c3c07338543e4016e5bc781f946
AlphaniaMuthee <walphania2@gmail.com>
2020-04-12 16:55:57 +0300

    add profile service details

--- src/app/userprofile/service.service.ts
+++ src/app/userprofile/service.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+mport { Injectable } from '@angular/core';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import {environment } from '../../environments/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  userprofiles : any;
+  private Username : string;
 
-  constructor() { }
+
+  constructor( private http : HttpClient ) {
+    console.log("It's Alive !");
+    this.Username = "AlphaniaMuthee";
+  }
+
+
+  getuserprofile(){
+    let promise = new Promise((resolve)=>{
+    return this.http.get("https://api.github.com/users/" + this.Username + "?client_id =" + environment.clientId+ "&client_secret=" + environment.clientsecret).toPromise().then(profiles =>{
+      this.userprofiles = userprofiles
+      resolve()
+    })
+    })
+    return promise
+  }
+  updateuserprofile(username : string){
+    this.Username = username
+
+  }
 }
