4a78d7ee85a071afc5ac39954c7a1503316234ef
AlphaniaMuthee <walphania2@gmail.com>
2020-04-12 16:33:05 +0300

    update the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import {ServicesService} from '../app/userprofile/service.service'
+import { FormsModule } from '@angular/forms';
+import {RoutingModule} from './routing/routing.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RepoComponent } from './repo/repo.component';
-import { UserprofileComponent } from './userprofile/userprofile.component';
+import { ProfileComponent } from './userprofile/userprofile.component';
 import { FormComponent } from './form/form.component';
-import { RouteComponent } from './route/route.component';
-import { ValidityComponent } from './validity/validity.component';
-import { RepositoryComponent } from './repository/repository.component';
-import { CountPipe } from './count.pipe';
+import { RepositoriesComponent } from './repository/repository.component';
+import { InvalidComponent } from './validity/validity.component';
 import { UnderlineDirective } from './underline.directive';
+import { CountPipe } from './count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepoComponent,
-    UserprofileComponent,
+    ProfileComponent,
     FormComponent,
-    RouteComponent,
-    ValidityComponent,
-    RepositoryComponent,
-    CountPipe,
-    UnderlineDirective
+    RepositoriesComponent,
+    InvalidComponent,
+    UnderlineDirective,
+    CountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RoutingModule,
   ],
-  providers: [],
+  providers: [ServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
